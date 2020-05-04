0379a75814f1c5055a2bc6567d7fd07f6aba2f64
Asadej Worrarojchalanthorn <asadej.w@gmail.com>
2020-05-04 15:23:15 +0700

    move git to git hub

--- src/public/class/Authen.php
+++ src/public/class/Authen.php
@@ -6,6 +6,7 @@ class Authen
     public static function middleware($app, $server)
     {
         return function ($request, $response, $next) use ($server) {
+            print_r(\OAuth2\Request::createFromGlobals());exit;
             if (!$server->verifyResourceRequest(\OAuth2\Request::createFromGlobals())) {
                 http_response_code(401);
                 header('Content-type:application/json');
--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,5 @@
 <?php
+// list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':' , base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
 date_default_timezone_set("Asia/Bangkok");
 require __DIR__ . '/../../vendor/autoload.php';
 
